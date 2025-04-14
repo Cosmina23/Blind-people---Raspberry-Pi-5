61f643d75f2bd614de49f6a21495a2b2a2850146
cosmina23 <cosmina.tunea@student.upt.ro>
2025-04-14 11:22:52 +0100

    change tts

--- FILES/textToSpeech.py
+++ FILES/textToSpeech.py
@@ -1,24 +1,16 @@
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"  # Ascunde mesajul de întâmpinare
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1" 
 
 from gtts import gTTS
 import pygame
 
 def speak_text(text):
-    # Creează obiectul gTTS
     tts = gTTS(text=text, lang='ro')
-
-    # Salvează fișierul audio
     tts.save("output.mp3")
 
-    # Inițializează pygame mixer
     pygame.mixer.init()
-
-    # Încarcă și redă fișierul audio
     pygame.mixer.music.load("output.mp3")
     pygame.mixer.music.play()
-
-    # Așteaptă până când fișierul audio este terminat
     while pygame.mixer.music.get_busy():
         continue
 
